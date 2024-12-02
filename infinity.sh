ae0f386d6128c826df0b1d8001bbc81935fef198
Sachin_07 <106669438+Sachinpawar86@users.noreply.github.com>
2024-12-02 21:37:15 +0530

    Update infinity.sh

--- infinity.sh
+++ infinity.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Remove the local manifests directory if it exists (cleanup before repo initialization)
-# rm -rf .repo/local_manifests/
+rm -rf .repo/local_manifests/
 
 # remove device tree
 rm -rf device/xiaomi/mojito
@@ -11,43 +11,33 @@ rm -rf vendor/xiaomi/sm6150-common
 rm -rf hardware/xiaomi
 
 # Initialize ROM manifest
-repo init -u https://github.com/EverestOS-AOSP/manifest -b 15 --git-lfs
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 15 -g default,-mips,-darwin,-notdefault
 
 # Sync the repo with force to ensure a clean sync
-repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
-#/opt/crave/resync.sh
+/opt/crave/resync.sh
 
 # remove frameworks/native
 # rm -rf frameworks/native
 
 # cloning device tree
-git clone https://github.com/K4LCHAKRA/android_device_xiaomi_mojito.git --depth 1 -b 15 device/xiaomi/mojito
-git clone https://github.com/K4LCHAKRA/android_device_xiaomi_sm6150-common.git --depth 1 -b 15 device/xiaomi/sm6150-common
+git clone https://github.com/InfinityA15/device_xiaomi_mojito.git --depth 1 -b 15 device/xiaomi/mojito
+git clone https://github.com/InfinityA15/android_device_xiaomi_sm6150-common.git --depth 1 -b 15 device/xiaomi/sm6150-common
 
 # cloning kernel tree
-git clone https://github.com/ProjectEverest-Devices/android_kernel_xiaomi_mojito.git --depth 1 -b inline-rom kernel/xiaomi/mojito
+git clone https://github.com/InfinityA15/kernel_xiaomi_mojito.git --depth 1 -b inline-rom kernel/xiaomi/mojito
 
 # cloning vendor tree
-git clone https://gitlab.com/bliss-mojito/android_vendor_xiaomi_mojito.git --depth 1 -b 14 vendor/xiaomi/mojito
-git clone https://gitlab.com/bliss-mojito/android_vendor_xiaomi_sm6150-common.git --depth 1 -b 14 vendor/xiaomi/sm6150-common
+git clone https://gitlab.com/sachinbarange86/android_vendor_xiaomi_mojito.git --depth 1 -b 14 vendor/xiaomi/mojito
+git clone https://gitlab.com/sachinbarange86/android_vendor_xiaomi_sm6150-common.git --depth 1 -b 14 vendor/xiaomi/sm6150-common
 
 # cloning hardware tree
-git clone https://github.com/ProjectEverest-Devices/android_hardware_xiaomi.git --depth 1 -b mojito hardware/xiaomi
-
-# source patch
-# git clone https://github.com/everestos-mojito/frameworks_native.git --depth 1 -b 14 frameworks/native
-
-# signing key for everestos
-git clone https://github.com/mojito-keys/vendor_lineage_signing.git --depth 1 -b main vendor/lineage
+git clone https://github.com/InfinityA15/android_hardware_xiaomi.git --depth 1 -b mojito hardware/xiaomi
 
 # Set up the build environment
 . build/envsetup.sh
 
-# out dir for pc
-export OUT_DIR=/media/dpenra/romout/everestout
-
 # Choose the target device
-lunch everest_mojito-ap3a-userdebug
+lunch infinity_mojito-userdebug
 
 # Build the ROM (use mka bacon for a full build)
-mka everest -j$(nproc --all)
+mka bacon
